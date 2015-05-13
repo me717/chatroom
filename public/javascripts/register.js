9d1a1170bc65bddbee213a4054a1b216bb890f68
me717 <albertmorlan@gmail.com>
2015-05-12 20:38:25 -0400

    Add message if username is taken during registration

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -11,10 +11,15 @@ function register() {
             method: "POST",
             data: data,
             success: function(response) {
-
+                if(!response) {
+                    $("#message").html("Username is already in use");
+                    $("#passwordInput").val("");
+                    $("#confirmInput").val("")
+                }
             },
             error: function(error) {
-                
+                $("#message").html("Server error.  Please try again later");
+                $("#passwordInput").val("");
             }
         });
     } else {
